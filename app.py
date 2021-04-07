8abf82df3737d1a28c3a7023095099423b3f5386
vanga <a.vanga@f5.com>
2021-04-07 17:58:19 +0530

    Added redirect feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from flask_cors import CORS
 import sqlite3
 from sqlite3 import OperationalError
@@ -9,7 +9,7 @@ counter = 1
 host = 'http://localhost:5000/'
 
 def init():
-    conn = sqlite3.connect('urls.db')
+    conn = sqlite3.connect('database.db')
     c = conn.cursor()
     # Create table - URL_MAP
     try:
@@ -25,8 +25,7 @@ def home():
     if request.method == 'POST':
         input = request.form
         url = input["url"]
-
-        with sqlite3.connect('urls.db') as conn:
+        with sqlite3.connect('database.db') as conn:
             cursor = conn.cursor()
             res = cursor.execute(
                 'SELECT SHORT_URL FROM URL_MAP WHERE URL LIKE ?', (url,))
@@ -37,7 +36,6 @@ def home():
             else:
                 cursor.execute('SELECT COUNT(*) from URL_MAP')
                 cur_result = cursor.fetchone()
-                print(cur_result[0])
                 if cur_result[0] == 0:
                     counter = 0
                 else:
@@ -53,6 +51,22 @@ def home():
 
     return render_template('index.html')
 
+@app.route('/<short_url>')
+def redirect_short_url(short_url):
+    try:
+        with sqlite3.connect('database.db') as conn:
+            cursor = conn.cursor()
+            res = cursor.execute('SELECT URL FROM URL_MAP WHERE SHORT_URL LIKE ?', (short_url,))
+            try:
+                original_url = res.fetchone()
+                if original_url is not None:
+                    return redirect(original_url[0])
+            except Exception as e:
+                print(e)
+        return redirect(host)
+
+    except OverflowError as e:
+        print(str(e))
 
 def encode(id):
     base62_characters = "0123456789abcdefghijkhlmnopgrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
